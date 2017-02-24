e39154f0a0d2c21168d01dbd39ffa74da1b9f514
Liu Dong <liudong@creditx.com>
2017-02-24 11:24:36 +0800

    test for dataMapper

--- work/tests/test_binaryClassifier.py
+++ work/tests/test_binaryClassifier.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 # encoding: utf-8
+import sys
+sys.path.append("../../")
+
 from work.marvin.binary_classifier_models.modelfit import BinaryClassifier, HyperOpt
 import work.marvin.binary_classifier_models
 from sklearn.datasets import make_classification
 from skopt.space import Categorical
 from sklearn.ensemble import RandomForestClassifier
+
+from work.marvin.dataPrepareforTraining.dataMapperPrepare import dataMapperBuilder
 
 
 def testBinaryClassifier():
@@ -16,6 +21,8 @@ def testBinaryClassifier():
     assert lr.model is work.marvin.binary_classifier_models.bestLrModelProducer
     gbm = BinaryClassifier("GBM")
     assert gbm.model is work.marvin.binary_classifier_models.bestGbdtModelProducer
+    lgb = BinaryClassifier("LightGBM")
+    assert lgb.model is work.marvin.binary_classifier_models.bestLightgbmModelProducer
 
 def testOptimizeModel():
     X, y = make_classification(n_samples=100, n_features=20, n_informative=2)
@@ -23,4 +30,10 @@ def testOptimizeModel():
                   'min_samples_leaf': (50, 500),
                   'min_samples_split': (50, 500),
                   'n_estimators': (50, 800)}
-    res = HyperOpt('RF').search(X, y, RandomForestClassifier, skopt_grid, 'neg_log_loss', n_calls=10)
+    for mod in ["RF", "GBRT", "GP"]:       
+        res = HyperOpt('RF').search(X, y, RandomForestClassifier, skopt_grid, 'neg_log_loss', n_calls=10)
+        assert len(res) == 10
+
+
+
+
--- work/tests/test_dataMapperPrepare.py
+++ work/tests/test_dataMapperPrepare.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # encoding: utf-8
+import sys
+sys.path.append("../../")
 import pytest
 import work.marvin.dataPrepareforTraining.dataMapperPrepare as mapperBuilder
+from sklearn2pmml.decoration import ContinuousDomain, CategoricalDomain
+import joblib
+import pandas as pd
 
 def test_FtrTransFunc():
     f = mapperBuilder.FtrTransFunc('MinMaxScaler')
@@ -9,3 +14,26 @@ def test_FtrTransFunc():
     assert f is mapperBuilder.FtrTransFunc.MIN_MAX_SCALER
     with pytest.raises(ValueError):
         mapperBuilder.FtrTransFunc('NewTransform')
+
+def test_dataMapperBuilder():
+    mis_val = {"request_id": ("dropRow", "active")}
+    traindf = joblib.load("fixtures/traindf")
+    assert isinstance(traindf, pd.DataFrame)
+    conti_ftr = traindf.describe().columns
+    categ_ftr = [ "request_id"]
+    print(len(conti_ftr))
+    print(len(categ_ftr), categ_ftr)
+    dfm = mapperBuilder.dataMapperBuilder(traindf, categ_ftr, conti_ftr, mis_val=mis_val)
+    print(traindf.columns.difference(["Label"]))
+    # ans = dfm.fit_transform(traindf[traindf.columns.difference(["Label"])], traindf["Label"])
+    ans = dfm.fit_transform(traindf, traindf["Label"])
+    print(ans.columns)
+
+
+def test_naming_hump_to_underline():
+    samples = [["asMising", "as_mising"], ["ming", "ming"], ["thereAreTwo", "there_are_two"]]
+    for ls in samples:
+        assert mapperBuilder.naming_hump_to_underline(ls[0]) == ls[1]
+
+
+test_dataMapperBuilder()
