55096823bd6f515c3f9bd6446709cae213b1db33
Wei Yan <weiyan@creditx.com>
2017-03-10 13:24:39 +0800

    featureselection bugfix; only support numpy array

--- work/tests/test_dataMapperPrepare.py
+++ work/tests/test_dataMapperPrepare.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # encoding: utf-8
-import sys
-sys.path.append("../../")
+from sklearn2pmml.decoration import ContinuousDomain, CategoricalDomain
+from numpy.testing import assert_array_equal
 import pytest
 import work.marvin.dataPrepareforTraining.dataMapperPrepare as mapperBuilder
-from sklearn2pmml.decoration import ContinuousDomain, CategoricalDomain
+import work.marvin.dataPrepareforTraining.featureSelection as featureSelection
 import joblib
+import numpy as np
 import pandas as pd
+import sys
+sys.path.append("../../")
 
 def test_FtrTransFunc():
     f = mapperBuilder.FtrTransFunc('MinMaxScaler')
@@ -23,15 +26,17 @@ def test_dataMapperBuilder():
     assert isinstance(traindf, pd.DataFrame)
     conti_ftr = traindf.describe().columns
     categ_ftr = [ "request_id"]
-    print(len(conti_ftr))
-    print(len(categ_ftr), categ_ftr)
     dfm = mapperBuilder.dataMapperBuilder(traindf, categ_ftr, conti_ftr, mis_val=mis_val)
-    print(traindf.columns.difference(["Label"]))
     ans = dfm.fit_transform(traindf[traindf.columns.difference(["Label"])], traindf["Label"])
     assert len(ans.columns) == 2
 
 
-def test_naming_hump_to_underline():
-    samples = [["asMising", "as_mising"], ["ming", "ming"], ["thereAreTwo", "there_are_two"]]
-    for ls in samples:
-        assert mapperBuilder.naming_hump_to_underline(ls[0]) == ls[1]
+def test_contifeatureimportance():
+    traindf = pd.DataFrame({"request_id": [0, 1, 2],
+                            "data": [1, 0, 0],
+                            "Label": [1, 0, 0]})
+    featureImpr = featureSelection.continuousFeatureImpr(traindf, "Label")
+    assert len(featureImpr) == 2
+    print(featureImpr.ix[0, :].tolist())
+    assert_array_equal(featureImpr.ix[0, :].tolist(), [np.inf, "data", 0.0, 0.866025403784])
+
