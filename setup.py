94c7e7da87c46e6fecccc1d27abb95a178f23c36
Wei Yan <weiyan@creditx.com>
2017-03-21 09:48:38 +0800

    bump version to 0.0.8

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ DESCRIPTION = "rich help module for marvin modelling"
 AUTHOR = "CreditX"
 AUTHOR_EMAIL = "marvin@creditx.com"
 URL = "https://git.creditx.com/marvin/marvin_modeling"
-VERSION = '0.0.7'
+VERSION = '0.0.8'
 
 setup(
     name=NAME,
